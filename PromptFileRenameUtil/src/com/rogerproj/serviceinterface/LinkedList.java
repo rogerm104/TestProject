0226ec507c56732b0c8aa45ba4c1d6bf51bea7ac
roger.m <rogerm104@gmail.com>
2020-01-30 22:47:52 +0530

    addded.

--- PromptFileRenameUtil/src/com/rogerproj/serviceinterface/LinkedList.java
+++ PromptFileRenameUtil/src/com/rogerproj/serviceinterface/LinkedList.java
@@ -38,6 +38,49 @@ public class LinkedList {
 		}
 		while(n.next!=null);
 		logNow.debug("The current data is "+ n.Data);
+	}
+
+	public Phone DeleteNode(Phone data){
+		Node thisNodes =currentNodes;
+		Node PreviousNode= null;
+		Node NextNextNode= null;
+
+		boolean found=false;
+		if(data instanceof Phone){
+			if(thisNodes!=null){
+				while(thisNodes.next!=null && !found){
+					if(thisNodes.Data.equals(data)){
+						found=true;
+						if(PreviousNode==null && thisNodes.next==null){
+							//this is the only node in the
+							currentNodes=null;
+							logNow.debug("Removed the only element in the LinkedList , Linked list is empty now!");
+						}else if(PreviousNode==null & thisNodes.next!=null) {
+							thisNodes.next=null;
+						}else if(PreviousNode!=null) {
+							//get the next-next node
+							PreviousNode.next=NextNextNode;
+						}
+					}
+					PreviousNode=thisNodes.next;
+					thisNodes=thisNodes.next;
+				}
+				//ToDo when while finished executing
+				NextNextNode=  thisNodes.next;
+
+			}else {
+				logNow.debug("No data in LinkedList");
+			}
+
+		}else {
+			logNow.debug("Data is not instance of Phone/ invalid data");
+		}
+		return null;
 
 	}
 }
+
+//n0|n1|n2|n3|n4
+//case 1 //n0.next=n2 [n.data = n+2]
+//case 2 //n0 is the only element[[ just set current node to null]]
+//case 3 //n0 is the last entry  [Previous node set next =null]
